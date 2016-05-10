ad898fd9b2568618b29404226bef55825dd51a35
Lun <famibu951217@hotmail.co.jp>
2016-05-11 01:57:08 +0900

    make it better

--- app.js
+++ app.js
@@ -6,23 +6,3 @@ var io = require('socket.io').listen(http);
 app.use(express.static(__dirname+"/public"));
 http.listen(process.env.PORT || 3000);
 console.log("server starting");
-
-io.sockets.on("connection",function(socket){
-	//投稿
-	socket.on("emit_from_client_tweet",function(data){
-		if(data.msg!=""){
-			socket.broadcast.to(data.room).emit("emit_from_server","["+data.name+"] : "+data.msg);
-			socket.emit("emit_from_server","["+data.name+"] : "+data.msg);
-			console.log(data.name+" : "+data.msg);
-		}
-	});
-	//部屋参加
-	socket.on("emit_from_client_in",function(data){
-		socket.join(data.room);
-		socket.emit("emit_from_server",data.name+"さんがログインしました");
-		socket.broadcast.to(data.room).emit("emit_from_server",data.name+"さんがログインしました");
-		socket.emit("inroom",data.name);
-		socket.emit("inroom2",data.room);
-	});
-
-});
