1127dbe381243e5f98ba60ea01bf2a708c08d979
Lun <famibu951217@hotmail.co.jp>
2016-12-20 00:10:28 +0900

    add noise but not implementation

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require("express"),
 	app = express(),
 	http = require("http").Server(app);
-var io = require('socket.io').listen(http);
 
 app.use(express.static(__dirname+"/public"));
 http.listen(process.env.PORT || 3000);
