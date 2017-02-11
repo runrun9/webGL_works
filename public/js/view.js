9418625417ac816414ddd37bbfc776b3d5b196a6
Lun <famibu951217@hotmail.co.jp>
2017-02-12 02:54:23 +0900

    hide address bar

--- public/js/view.js
+++ public/js/view.js
@@ -15,7 +15,7 @@
   loop();
 
   function init() {
-    setTimeout(scrollTo, 300, 0, 1);
+    setTimeout(scrollTo, 300, 0, 100);
     world = document.getElementById('world');
       // シーンの作成
       scene = new THREE.Scene();
